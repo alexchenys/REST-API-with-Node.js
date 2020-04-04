6d093edef9d9e5e1f531ebefb2038788c73f423b
Chen,Yu-Shiuan <yu.shiuan2017@gmail.com>
2020-04-04 20:10:09 +0800

    Add more routes to the API

---
+++ api/routes/order.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+router.get('/:orderID', function(req, res, next){
+    res.status(200).json({
+        message: 'Orders were fetched',
+        orderID : req.params.orderID
+    });
+});
+
+router.delete('/:orderID', function(req, res, next){
+    res.status(200).json({
+        message: 'Orders deleted',
+        orderID : req.params.orderID
+    });
+});
+
+router.post('/', function(req, res, next){
+    res.status(201).json({
+        message: 'Order was created'
+    })
+})
+module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    })
-})
-
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/order');
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 module.exports = app;
 
 
