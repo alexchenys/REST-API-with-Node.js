9fc13327e108487d45f51d0b0b225f6727db9350
Chen,Yu-Shiuan <yu.shiuan2017@gmail.com>
2020-04-04 21:28:27 +0800

    Handling Errors & Improving the Project Setup

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use(function(req, res, next){
+    const error = new Error('Not found')
+    error.status(404);
+    next(error)
+})
+app.use(function(error, req, res, next){
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
 module.exports = app;
 
 
